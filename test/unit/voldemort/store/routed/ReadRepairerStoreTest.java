a35a0d70a2f0dc0be8219204b97a898d9b86926c
Mark Rambacher <mark.rambacher@nokia.com>
2010-06-19 07:25:41 -0400

    Fix issues with Read Repair on concurrent versions

---
+++ test/unit/voldemort/store/routed/ReadRepairerStoreTest.java
@@ -0,0 +1,121 @@
+package voldemort.store.routed;
+
+import static voldemort.FailureDetectorTestUtils.recordException;
+import static voldemort.FailureDetectorTestUtils.recordSuccess;
+import static voldemort.MutableStoreVerifier.create;
+import static voldemort.cluster.failuredetector.FailureDetectorUtils.create;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import voldemort.MockTime;
+import voldemort.ServerTestUtils;
+import voldemort.TestUtils;
+import voldemort.VoldemortTestConstants;
+import voldemort.cluster.Cluster;
+import voldemort.cluster.failuredetector.BannagePeriodFailureDetector;
+import voldemort.cluster.failuredetector.FailureDetector;
+import voldemort.cluster.failuredetector.FailureDetectorConfig;
+import voldemort.routing.RoutingStrategyType;
+import voldemort.store.Store;
+import voldemort.store.StoreDefinition;
+import voldemort.store.memory.InMemoryStorageEngine;
+import voldemort.utils.ByteArray;
+import voldemort.utils.Time;
+import voldemort.versioning.Versioned;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
+
+@RunWith(Parameterized.class)
+public class ReadRepairerStoreTest extends TestCase {
+
+    private final Class<FailureDetector> failureDetectorClass;
+    private FailureDetector failureDetector;
+    private Time time = new MockTime();
+
+    public ReadRepairerStoreTest(Class<FailureDetector> failureDetectorClass) {
+        this.failureDetectorClass = failureDetectorClass;
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        if(failureDetector != null)
+            failureDetector.destroy();
+    }
+
+    @Parameters
+    public static Collection<Object[]> configs() {
+        return Arrays.asList(new Object[][] { { BannagePeriodFailureDetector.class } });
+    }
+
+    /**
+     * See Issue 150: Missing keys are not added to node when performing
+     * read-repair
+     */
+
+    @Test
+    public void testMissingKeysAreAddedToNodeWhenDoingReadRepair() throws Exception {
+        ByteArray key = TestUtils.toByteArray("key");
+        byte[] value = "foo".getBytes();
+
+        Cluster cluster = VoldemortTestConstants.getThreeNodeCluster();
+        StoreDefinition storeDef = ServerTestUtils.getStoreDef("test",
+                                                               3,
+                                                               3,
+                                                               3,
+                                                               2,
+                                                               2,
+                                                               RoutingStrategyType.CONSISTENT_STRATEGY);
+        Map<Integer, Store<ByteArray, byte[]>> subStores = Maps.newHashMap();
+        for(int a = 0; a < 3; a++) {
+            subStores.put(Iterables.get(cluster.getNodes(), a).getId(),
+                          new InMemoryStorageEngine<ByteArray, byte[]>("test"));
+        }
+
+        FailureDetectorConfig failureDetectorConfig = new FailureDetectorConfig().setImplementationClassName(failureDetectorClass.getName())
+                                                                                 .setBannagePeriod(1000)
+                                                                                 .setNodes(cluster.getNodes())
+                                                                                 .setStoreVerifier(create(subStores))
+                                                                                 .setTime(time);
+
+        failureDetector = create(failureDetectorConfig, false);
+
+        RoutedStore store = new RoutedStore("test",
+                                            subStores,
+                                            cluster,
+                                            storeDef,
+                                            1,
+                                            true,
+                                            1000L,
+                                            failureDetector);
+
+        recordException(failureDetector, Iterables.get(cluster.getNodes(), 0));
+        store.put(key, new Versioned<byte[]>(value));
+        recordSuccess(failureDetector, Iterables.get(cluster.getNodes(), 0));
+        time.sleep(2000);
+
+        assertEquals(2, store.get(key).size());
+        // Last get should have repaired the missing key from node 0 so all
+        // stores should now return a value
+        assertEquals(3, store.get(key).size());
+
+        ByteArray anotherKey = TestUtils.toByteArray("anotherKey");
+        // Try again, now use getAll to read repair
+        recordException(failureDetector, Iterables.get(cluster.getNodes(), 0));
+        store.put(anotherKey, new Versioned<byte[]>(value));
+        recordSuccess(failureDetector, Iterables.get(cluster.getNodes(), 0));
+        assertEquals(2, store.getAll(Arrays.asList(anotherKey)).get(anotherKey).size());
+        assertEquals(3, store.get(anotherKey).size());
+    }
+}
